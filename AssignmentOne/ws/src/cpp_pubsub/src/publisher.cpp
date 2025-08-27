f953b7af73d55f3abd8c4694812d4a186e4fc669
electriccheffer <electriccheff@gmail.com>
2025-08-27 09:54:58 -0500

    IMPLEMENTED SmallPublisher.cpp

---
+++ AssignmentOne/ws/src/cpp_pubsub/src/publisher.cpp
@@ -0,0 +1,42 @@
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "rclpp/rclpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+using namespace std::chrono_literals
+
+class SmallPublisher : public rclpp::Node{
+
+
+	public: SmallPublisher():Node("small_publisher"),count_(0){
+		
+
+		publisher_ = this->create_publisher<std_msgs::msg::String>("topic",10);
+		auto timer_callback = [this]()-> void {
+				
+			auto message = std_msgs::msg::String();
+			message.data = "Hello, world! " + std::to_string(this->count_++);
+			RCLCPP_INFO(this->get_logger(),"Publishing: '%s'",message.data.c_str()); 
+			this->publisher_->publish(message); 
+		}; 
+		timer_ = this->create_wall_timer(500ms,timer_callback); 
+	
+	}
+	private: 
+		rclcpp::TimerBase::SharedPtr timer_; 
+		rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_; 
+		size_t count_; 
+}
+
+
+int main(int argc, char * argv[]){
+
+	rclcpp::init(argc,argv); 
+	rclcpp::spin(std::make_shared<SmallPublisher>()); 
+	rclcpp::shutdown(); 
+	return 0; 
+
+}
