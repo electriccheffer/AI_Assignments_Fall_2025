87d6f4005dc27d2cbae954551635288cd1a755ad
electriccheffer <electriccheff@gmail.com>
2025-08-27 10:51:11 -0500

    ASSIGNMENT ONE COMPLETE

--- AssignmentOne/ws/src/cpp_pubsub/src/publisher.cpp
+++ AssignmentOne/ws/src/cpp_pubsub/src/publisher.cpp
@@ -2,34 +2,35 @@
 #include <memory>
 #include <string>
 
-#include "rclpp/rclpp.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 
-using namespace std::chrono_literals
+using namespace std::chrono_literals; 
 
-class SmallPublisher : public rclpp::Node{
+class SmallPublisher : public rclcpp::Node{
 
 
-	public: SmallPublisher():Node("small_publisher"),count_(0){
+	public: 
+		SmallPublisher():Node("small_publisher"),count_(0){
 		
 
-		publisher_ = this->create_publisher<std_msgs::msg::String>("topic",10);
-		auto timer_callback = [this]()-> void {
-				
-			auto message = std_msgs::msg::String();
-			message.data = "Hello, world! " + std::to_string(this->count_++);
-			RCLCPP_INFO(this->get_logger(),"Publishing: '%s'",message.data.c_str()); 
-			this->publisher_->publish(message); 
-		}; 
-		timer_ = this->create_wall_timer(500ms,timer_callback); 
-	
+			publisher_ = this->create_publisher<std_msgs::msg::String>("topic",10);
+			auto timer_callback = [this]()-> void {
+					
+				auto message = std_msgs::msg::String();
+				message.data = "Hello, world! " + std::to_string(this->count_++);
+				RCLCPP_INFO(this->get_logger(),"Publishing: '%s'",message.data.c_str()); 
+				this->publisher_->publish(message); 
+			}; 
+			timer_ = this->create_wall_timer(500ms,timer_callback); 
+		
 	}
 	private: 
 		rclcpp::TimerBase::SharedPtr timer_; 
 		rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_; 
 		size_t count_; 
-}
+};
 
 
 int main(int argc, char * argv[]){
